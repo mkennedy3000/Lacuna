59a4430718a50d40d31f6014109d6b154ea1987c
Mark Kennedy <mkennedy3000@gmail.com>
2013-02-09 22:50:25 -0600

    Button Class Added

---
+++ Button.lua
@@ -0,0 +1,86 @@
+--------------------
+--[ Button Class ]--
+--------------------
+
+Button = {
+	text = "Button",
+	size = 32,
+	x = 0,
+	y = 400, 
+	align = "center",
+	color = { r = 191, g = 56, b = 26 }, --RedOrange
+	hoverColor = { r = 10, g = 34, b = 78 }, --DarkBlue
+	pressedColor = { r = 160, g = 216, b = 241 }, --LightBlue
+	hitBox = { w = 100, h = 40 },
+	callBack = function() end --placeholder
+}
+
+--------------------------------
+--[ Private Helper Functions ]--
+--------------------------------
+
+
+----------------------
+--[ Button Methods ]--
+----------------------
+--[[
+ 	new
+ 	Button Constructor
+ --]]
+function Button:new (o)
+	o = o or {}		--create table if user does not provide one
+	
+	--Initialize Variables--
+	o.hoverOver = false
+	o.pressed = false
+	
+	setmetatable(o, self)
+	self.__index = self
+	return o
+end
+
+function Button:update()
+	--Check if mouse is hovering over button--
+	local x, y = love.mouse.getPosition()
+
+	if x >= 250-(self.hitBox.w/2) and --250 is the screen width, should load into constants...
+	   x <= 250+(self.hitBox.w/2) and
+	   y >= self.y and
+	   y <= self.y+self.hitBox.h then
+		self.hoverOver = true
+	else
+		self.hoverOver = false
+	end
+	
+	--Check for button presses--
+	if love.mouse.isDown("l") then
+		if self.hoverOver then
+			self.pressed = true
+		else
+			self.pressed = false
+		end
+		
+	--Check for button releases--
+	else
+		if self.pressed then
+			--Button Clicked--
+			self.pressed = false
+			self.callBack()
+		end
+	end
+end
+
+function Button:draw()
+	if self.hoverOver == true then
+		if not self.pressed then
+			love.graphics.setColor(self.hoverColor.r, self.hoverColor.g, self.hoverColor.b)
+		else
+			love.graphics.setColor(self.pressedColor.r, self.pressedColor.g, self.pressedColor.b)
+		end
+		love.graphics.rectangle("fill", 250-(self.hitBox.w/2), self.y, self.hitBox.w, self.hitBox.h)
+	end
+	
+	love.graphics.setColor(self.color.r, self.color.g, self.color.b)
+	love.graphics.setNewFont(self.size)
+	love.graphics.printf(self.text, self.x, self.y, 500, self.align)
+end
--- menuScene.lua
+++ menuScene.lua
@@ -41,45 +41,7 @@ function drawMenu()
 	menuPlayButton:draw()
 	
 	--Draw Quit Button--
-<<<<<<< HEAD
-	if menuQuitButton.hitBox.inRange == true then
-		if not menuQuitButton.hitBox.pressed then
-			love.graphics.setColor(menuQuitButton.hoverColor.r, menuQuitButton.hoverColor.g, menuQuitButton.hoverColor.b)
-		else
-			love.graphics.setColor(menuQuitButton.pressedColor.r, menuQuitButton.pressedColor.g, menuQuitButton.pressedColor.b)
-		end
-		love.graphics.rectangle("fill", 250-(menuQuitButton.hitBox.w/2), menuQuitButton.y, menuQuitButton.hitBox.w, menuQuitButton.hitBox.h)
-	end
-	
-	love.graphics.setColor(menuQuitButton.color.r, menuQuitButton.color.g, menuQuitButton.color.b)
-	love.graphics.setNewFont(menuPlayButton.size)
-	love.graphics.printf(menuQuitButton.text, menuQuitButton.x, menuQuitButton.y, 500, menuQuitButton.align)
-	
-end
-
-function isHovering(x,y)
-	--Play Button--
-	if x >= 250-(menuPlayButton.hitBox.w/2) and
-	   x <= 250+(menuPlayButton.hitBox.w/2) and
-	   y >= menuPlayButton.y and
-	   y <= menuPlayButton.y+menuPlayButton.hitBox.h then
-		menuPlayButton.hitBox.inRange = true
-	else
-		menuPlayButton.hitBox.inRange = false
-	end
-	
-	--Quit Button--
-	if x >= 250-(menuQuitButton.hitBox.w/2) and
-	   x <= 250+(menuQuitButton.hitBox.w/2) and
-	   y >= menuQuitButton.y and
-	   y <= menuQuitButton.y+menuQuitButton.hitBox.h then
-		menuQuitButton.hitBox.inRange = true
-	else
-		menuQuitButton.hitBox.inRange = false
-	end
-=======
 	menuQuitButton:draw()
->>>>>>> Button class added
 end
 
 function deleteMenu()
